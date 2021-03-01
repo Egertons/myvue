15b21b6ea2b8ce0f51f29f082e73e8941933716e
egert <egertons@163.com>
2021-03-01 17:06:27 +0800

    2021_3_1_17_06

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  //配置路由（启用路由）
+  router,
+  components: { App },
+  template: '<App/>'
+})
